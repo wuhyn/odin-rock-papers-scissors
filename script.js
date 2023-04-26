184d651295c3a0a4d88b9e7759b99c1607d8781d
Hong Ng <hong.yang.ng@gmail.com>
2023-04-26 18:10:28 +1000

    Add playRound function to implement rock, paper and scissors logic

--- script.js
+++ script.js
@@ -9,4 +9,47 @@ function getComputerChoice(){
     return choices[randomNumber];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    //Ensure player selection string is case-insensitive
+    let playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    let computerChoice = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+
+    console.log(playerChoice);
+    console.log(computerChoice);
+
+    //If player and computer selection is the same, draw, replay
+    if(playerChoice == computerChoice){
+        return "Draw";
+    }
+    //If player chooses rock
+    else if(playerChoice == "Rock"){
+        if(computerChoice == "Paper"){
+            return "You Lose! " + computerChoice + " beats " + playerChoice;
+        }
+        else if(computerChoice == "Scissors"){
+            return "You Win! " + playerChoice + " beats " + computerChoice;
+        }
+    }
+    //If player chooses paper
+    else if(playerChoice == 'Paper'){
+        if(computerChoice == "Scissors"){
+            return "You Lose! " + computerChoice + " beats " + playerChoice;
+        }
+        else if(computerChoice == "Rock"){
+            return "You Win! " + playerChoice + " beats " + computerChoice;
+        }
+    }
+    //If player chooses scissors
+    else{
+        if(computerChoice == "Rock"){
+            return "You Lose! " + computerChoice + " beats " + playerChoice;
+        }
+        else if(computerChoice == "Paper"){
+            return "You Win! " + playerChoice + " beats " + computerChoice;
+        }      
+    }
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
